bd5b5973b5098f0d4c508902910d30f39065b60d
usershanes <usersshane@outlook.com>
2025-03-28 22:34:54 +0800

    fix

--- fifth/stop_watch/lib/stopwatch.dart
+++ fifth/stop_watch/lib/stopwatch.dart
@@ -3,26 +3,29 @@ import 'package:flutter/material.dart';
 
 class StopWatch extends StatefulWidget {
   @override
-  State<StopWatch> createState() => StopWatchState();
+  State createState() => StopWatchState();
 }
 
 class StopWatchState extends State<StopWatch> {
   int seconds = 0;
+  bool isTicking = true;
   Timer? timer;
 
+  /*
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     seconds = 0;
-    timer = Timer.periodic(Duration( seconds: 10 ), _onTick);
+    timer = Timer.periodic(Duration( seconds: 1 ), _onTick);
   }
+
+   */
   void _onTick(Timer timer) {
     setState( () {
       ++seconds;
     });
   }
-
   String _secondsText() => seconds == 1 ? 'second' : 'seconds';
 
   @override
@@ -34,15 +37,54 @@ class StopWatchState extends State<StopWatch> {
 
   @override
   Widget build(BuildContext context) {
+    void _startTimer() {
+      timer = Timer.periodic(Duration( seconds: 1 ), _onTick );
+      setState(() {
+        isTicking = true;
+      });
+    }
+
+    void _stopTimer(){
+      timer?.cancel();
+      setState(() {
+        isTicking = false;
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          '$seconds ${_secondsText()}',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Text(
+            '$seconds ${_secondsText()}',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>( Colors.red ),
+                  foregroundColor: MaterialStateProperty.all<Color>( Colors.white ),
+                ),
+                child: Text('Start'),
+                onPressed: isTicking ? null : _startTimer,
+              ),
+              SizedBox(width: 20),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>( Colors.red ),
+                  foregroundColor: MaterialStateProperty.all<Color>( Colors.white ),
+                ),
+                child: Text('Stop'),
+                onPressed: isTicking ? _stopTimer : null,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
